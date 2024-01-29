c4ab004c2c159e6c1a326745426d3f9b68d95583
tomagon <agonnoudetom@gmail.com>
2024-01-29 15:11:55 +0100

    mise au propre

---
+++ app.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+import uvicorn
+import joblib
+import pandas as pd
+
+names_app = FastAPI()
+
+def encode_prenom(prenom: str) -> pd.Series:
+    """
+        This function encode a given name into a pd.Series.
+        
+        For instance alain is encoded [1, 0, 0, 0, 0 ... 1, 0 ...].
+    """
+    alphabet = "abcdefghijklmnopqrstuvwxyzé-'"
+    prenom = prenom.lower()
+    
+    return pd.Series([letter in prenom for letter in alphabet]).astype(int)
+
+@names_app.get("/predict")
+async def predict(name:str):
+    regr_loaded = joblib.load("model.v1.bin")
+    sexe = regr_loaded.predict([encode_prenom(name)])
+    sexe = ("M" if sexe[0]==0 else "F")
+
+    return {"name":name, "predicted_sex":sexe}
+
+
+if __name__ == "__main__":
+    uvicorn.run(names_app, host="0.0.0.0", port=5000)
+
